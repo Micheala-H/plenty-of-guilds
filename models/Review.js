e226f171f855415ac6cca441d71e22d9bd243236
rudyxwhite <brandonrwing98@gmail.com>
2023-03-22 08:41:33 -0500

    made models and associations

---
+++ models/Review.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {}
+
+Review.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true
+    },
+    rating: {
+      type: DataTypes.DECIMAL,
+      defaultValue: null,
+      validate: {
+        isNumeric: true
+      }
+    },
+    comment: {
+      type: DataTypes.TEXT,
+      defaultValue: null
+    },
+    character_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'character',
+        key:'id'
+      }
+    },
+    user_id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model:'user',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'review',
+  }
+);
+
+module.exports = Review;
--- models/index.js
+++ models/index.js
@@ -1,7 +1,40 @@
 const User = require('./User');
-// const  = require('');
-// const  = require('')
+const Character = require('./Character');
+const Review = require('./Review')
+
+Character.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Character, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Review.belongsTo(Character,{
+    foreignKey: 'character_id'
+})
+
+Character.hasMany(Review, {
+    foreignKey: 'character_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Review, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+Review.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 
 
-module.exports = User ;
+
+
+module.exports = {
+    User,
+    Character,
+    Review
+}
