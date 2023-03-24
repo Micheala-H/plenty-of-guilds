7b472f5fd30be6d821a015117a22834bb1bc11e4
rudyxwhite <brandonrwing98@gmail.com>
2023-03-23 19:20:40 -0500

    commit

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./characterRoutes');
 const commentRoutes = require('./route2');
+const characterRoutes = require('./characterRoutes')
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+router.use('./characters', characterRoutes);
 
 module.exports = router;
